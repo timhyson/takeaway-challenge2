0ee79afde82241f3eae53b09edd9086cc8d17924
Tim Hyson <tjhyson@gmail.com>
2015-09-16 17:17:43 +0100

    First Commit

---
+++ .env.rb
@@ -0,0 +1,5 @@
+ENV = { account_sid: "ACcea4fb40215f6f02a5e61f8bbf8c78d0",
+        auth_token:  "cc977d61ef8e5ae50fedd461321d1710",
+        recipient:   "07971693375",
+        twilio:      "+441604422181"
+      }
